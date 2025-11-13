e96b0f80473016e5e05bf202246959b04c13bce5
Cleverland197710 <realzacharyjayden@gmail.com>
2025-11-13 15:29:00 +0000

    1.11

---
+++ Assets/Scripts/Go through.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem; // <-- new namespace
+using UnityEngine.SceneManagement;
+
+public class Gothrough : MonoBehaviour
+{
+    private InputAction nextSceneAction;
+
+    void Awake()
+    {
+        // Create an InputAction that triggers when the Space key is pressed
+        nextSceneAction = new InputAction(binding: "<Keyboard>/space");
+        nextSceneAction.performed += ctx => LoadNextScene();
+    }
+
+    void OnEnable()
+    {
+        nextSceneAction.Enable();
+    }
+
+    void OnDisable()
+    {
+        nextSceneAction.Disable();
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
